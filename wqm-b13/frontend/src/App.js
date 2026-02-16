765a866dccfc18264a0f9b26ee3ff1046de5588f
vasihakolimi <vasihakolimi@gmail.com>
2026-02-16 23:13:18 +0530

    frontend

---
+++ wqm-b13/frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
